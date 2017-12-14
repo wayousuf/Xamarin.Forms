15a89973e6bedbd45b0b7698da503ed26dbb51e9
Waqas Yousuf <wyousuf@tsdweb.com>
2017-12-14 16:25:24 -0500

    Basic Example of Xamarin Form with MVVM

---
+++ Movies.MVVM/Movies.MVVM/Movies.MVVM/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Movies.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Movies.MVVM
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            BindingContext = new MainViewModel();
+			InitializeComponent();
+		}
+	}
+}
---
+++ Movies.MVVM/Movies.MVVM/Movies.MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Movies.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Movies.MVVM.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private IReadOnlyList<Movie> _movies;
+        public IReadOnlyList<Movie> Movies
+        {
+            get => _movies;
+            private set
+            {
+                _movies = value;
+                RaisePropertyChanged(nameof(Movies));
+            }
+        }
+
+        private string _searchTerm;
+        public string SearchTerm
+        {
+            get => _searchTerm;
+            set
+            {
+                if (_searchTerm != value)
+                {
+                    _searchTerm = value;
+                    RaisePropertyChanged();
+                    OnSearchTermChangeAsync(_searchTerm)
+                        .ContinueWith(tr => throw new Exception("Search Failed.", tr.Exception), 
+                        TaskContinuationOptions.OnlyOnFaulted);
+                }
+            }
+        }
+
+        private CancellationTokenSource _cts;
+        private async Task OnSearchTermChangeAsync(string searchTerm)
+        {
+            _cts?.Cancel();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                var innerToken = _cts = new CancellationTokenSource();
+
+                var movieService = new MovieService();
+                var movies = await movieService.GetMoviesForSearchAsync(_searchTerm);
+                if (!innerToken.IsCancellationRequested)
+                {
+                    Movies = movies;
+                }
+            }
+        }
+    }
+}
